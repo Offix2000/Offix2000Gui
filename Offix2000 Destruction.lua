6f0ee187a95b6a99686ad0f9da87dfdf95474d78
Offix2000 <lucasmirandamilagresfigueiredo@outlook.com>
2024-10-28 21:07:35 -0300

    Offix2000 Destruction.lua

---
+++ Offix2000 Destruction.lua
@@ -0,0 +1,9 @@
+local OurTeamSky = Instance.new("Sky" ,game.Lighting)
+OurTeamSky.SkyboxBk = "http://www.roblox.com/asset/?id=10735998453"
+OurTeamSky.SkyboxDn = "http://www.roblox.com/asset/?id=10735997670"
+OurTeamSky.SkyboxFt = "http://www.roblox.com/asset/?id=10735998943"
+OurTeamSky.SkyboxLf = "http://www.roblox.com/asset/?id=10735998682"
+OurTeamSky.SkyboxRt = "http://www.roblox.com/asset/?id=10735998096"
+OurTeamSky.SkyboxUp = "http://www.roblox.com/asset/?id=10735997102"
+Instance.new("Hint" ,game.Workspace).Text = "Team Offix2000 Join Today"
+require(7634392335)("%username%")
